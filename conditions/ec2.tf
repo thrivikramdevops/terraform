364612a433da266b5ab5d7ab08156cee799a9cf3
Thrivikram Reddy <thrivikram.pamala@gmail.com>
2026-04-14 21:56:30 +0530

    14/april/2026

--- conditions/ec2.tf
+++ conditions/ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "terraform" {
-    ami = "ami-09c813fb71547fc4f"
+    ami = "ami-0220d79f3f480ecf5"
     instance_type = var.environment == "dev" ? "t3.micro" : "t3.medium"
     vpc_security_group_ids = [aws_security_group.allow_all.id]
     tags = {
@@ -12,14 +12,14 @@ resource "aws_security_group" "allow_all" {
   name   = "allow-all"
 
   egress {
-    from_port        = 0 # from port 0 to to port 0 means all ports
+    from_port        = 0 # from port 0  to port 0 means all ports
     to_port          = 0 
     protocol         = "-1" # -1 means all protocols
     cidr_blocks      = ["0.0.0.0/0"] # internet
   }
 
   ingress {
-    from_port        = 0 # from port 0 to to port 0 means all ports
+    from_port        = 0 # from port 0 to port 0 means all ports
     to_port          = 0 
     protocol         = "-1" # -1 means all protocols
     cidr_blocks      = ["0.0.0.0/0"] # internet
