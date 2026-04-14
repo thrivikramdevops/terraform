a23f8e482ac11801fb1e3124d282a7540cc1aaf3
Thrivikram Reddy <thrivikram.pamala@gmail.com>
2026-04-14 21:09:44 +0530

    14/April/2026

--- variables/variables.tf
+++ variables/variables.tf
@@ -1,11 +1,11 @@
 variable "ami_id" {
     type = string
-    default     = "ami-09c813fb71547fc4f"
+    default     = "ami-0220d79f3f480ecf5"
 }
 
 variable "instance_type" {
     type = string
-    default = "t3.micro"
+    default = "t2.micro"
 }
 
 variable "ec2_tags" {
